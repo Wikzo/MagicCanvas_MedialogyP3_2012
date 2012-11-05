2b2a04457646d6ce51211ad3425e0c6c47ee975d
Wikzo <Linkthewindwaker@gmail.com>
2012-11-05 17:19:01 +0100

    sln and suo added

---
+++ MagicCanvas/MagicCanvas/Main.cpp
@@ -0,0 +1,17 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat im = imread("nisse.jpg");
+    if (im.empty()) 
+    {
+        cout << "Cannot load image!" << endl;
+        return -1;
+    }
+    imshow("Image", im);
+    waitKey(0);
+}
