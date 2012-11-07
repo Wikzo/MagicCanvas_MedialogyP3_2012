15cffac7a90bb5380f1557ac445ca0a39843a615
Wikzo <Linkthewindwaker@gmail.com>
2012-11-07 09:46:34 +0100

    threshold and mean filter

--- MagicCanvas/MagicCanvas/Main.cpp
+++ MagicCanvas/MagicCanvas/Main.cpp
@@ -1,5 +1,5 @@
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
 using namespace cv;
@@ -8,17 +8,17 @@ using namespace std;
 // Fighting Mongooses
 // Marco, Simon, Gustav
 
-// TODO:
-// Grab background
-// Grab current video
-// Subtract current video from background
-// Use local threshold and make binary
-// Remove noise with erosion
-// PROFIT!!!
+// Step 1: Save reference image (background)
+// Step 2: Capture current image (video)
+// Step 3: Perform image subtraction
+// Step 4: Threshold (local)
+// Step 5: Noise reduction - Mean filter or morphological opening/closing
 
 // Function prototypes
 void GetBackground(VideoCapture capture, Mat &backgroundToWriteTo);
 void BackgroundSubstracted(Mat currentFrame, Mat background);
+Mat Threshold(int threshold);
+Mat MeanFilter(Mat image, int threshold);
 
 int main()
 {
@@ -47,6 +47,9 @@ int main()
 		capture >> currentFrame;
 		if (currentFrame.empty())
 			break;
+
+		// Convert to grayscale
+		cvtColor(currentFrame, currentFrame, CV_RGB2GRAY);
 
 		// Exit
 		if ((char)waitKey(30) == 'q')
@@ -88,4 +91,68 @@ void BackgroundSubstracted(Mat currentFrame, Mat background)
 	Substration = currentFrame - background;
 	cvtColor(Substration, GrayScale_substraction, CV_RGB2GRAY);
 	*/
+}
+
+Mat Threshold(Mat image, int threshold)
+{
+	// TODO: Make local threshold (p. 125 in book)
+	// Use on binary image only
+
+	// Loop through all pixels and set them to either black (0) or white (255) using a threshold
+	for (int y = 0; y < image.rows; y++)
+	{
+		for (int x = 0; x < image.cols; x++)
+		{
+			int value = 0;
+			if (image.at<uchar>(y, x) < 0)
+				value = image.at<uchar>(y, x) * -1; // Take absolute value
+			else
+				value = image.at<uchar>(y, x);
+
+			if (value >= threshold)
+				image.at<uchar>(y, x) = 255;
+			else
+				image.at<uchar>(y, x) = 0;
+
+		}
+	}
+
+	return image;
+}
+
+Mat MeanFilter(Mat input)
+{
+	// 5x5 kernel
+
+	// Make a temporary clone of the input image
+	Mat mean = input.clone();
+
+	// Loop through all pixels
+	for (int y = 0; y < input.rows-2; y++)
+	{
+		for (int x = 0; x < input.cols-2; x++)
+		{
+			if (x - 2 < 0 || y - 2 < 0) // don't go out of bounds
+				continue;
+
+			// Apply the kernel
+			mean.at<uchar>(y, x) = (
+				input.at<uchar>(y-2, x-2) + input.at<uchar>(y-2, x-1)
+				+ input.at<uchar>(y-2, x) + input.at<uchar>(y-2, x+1)
+				+ input.at<uchar>(y-2, x+2) + input.at<uchar>(y-1, x-2)
+				+ input.at<uchar>(y-1, x-1) + input.at<uchar>(y-1, x)
+				+ input.at<uchar>(y-1, x+1) + input.at<uchar>(y-1, x+2)
+				+ input.at<uchar>(y, x-2) + input.at<uchar>(y, x-1)
+				+ input.at<uchar>(y, x) + input.at<uchar>(y, x+1)
+				+ input.at<uchar>(y, x+2) + input.at<uchar>(y+1, x-2)
+				+ input.at<uchar>(y+1, x-1) + input.at<uchar>(y+1, x)
+				+ input.at<uchar>(y+1, x+1) + input.at<uchar>(y+1, x+2)
+				+ input.at<uchar>(y+2, x-2)	+ input.at<uchar>(y+2, x-1)
+				+ input.at<uchar>(y+2, x) + input.at<uchar>(y+2, x+1)
+				+ input.at<uchar>(y+2, x+2)
+				) / 25;
+		}
+	}
+
+	return mean;
 }
