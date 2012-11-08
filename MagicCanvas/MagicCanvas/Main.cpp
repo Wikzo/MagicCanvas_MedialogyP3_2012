443b50faad9f5d766896871636b26525f6a47063
Wikzo <Linkthewindwaker@gmail.com>
2012-11-08 10:38:02 +0100

    merge test

--- MagicCanvas/MagicCanvas/Main.cpp
+++ MagicCanvas/MagicCanvas/Main.cpp
@@ -252,9 +252,10 @@ void Picture::findFirstRow(int minRowLength, int minRowWidth, point &startOfTheL
 }
 void Picture::drawPictureAt(point lowerLeftCorner, int newwidth, Picture pictureToDraw)
 {
+	// max bla bla bla
 	float sf = (float)pictureToDraw.width/newwidth;
 
-	int newheight = pictureToDraw.height/sf;
+	int newheight_gustav = pictureToDraw.height/sf;
 
 
 	cout << "x: " << lowerLeftCorner.x << " y: " << lowerLeftCorner.y << " width: " << newwidth << " height: " << newheight << " sf: " << sf <<"\n";
