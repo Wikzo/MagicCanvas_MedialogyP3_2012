68db907439f85574ccd9f919f2ebea5790798d28
tukituk <martabotellacampos@gmail.com>
2012-11-08 11:16:05 +0100

    test to merge without merge tool

---
+++ MagicCanvas/MagicCanvas/Main.cpp
@@ -0,0 +1,13 @@
+void Picture::contours(Picture refPic)
+{
+	for(int x = 0; x < refPic.width; x++)
+	{
+		for(int y = 0; y < refPic.height; y++)
+		{
+			if(refPic.pixel[x][y] = 255)
+			{
+
+			}
+		}
+	}
+}
